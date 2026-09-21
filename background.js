a45764d88d04ce11076427ff2bf5348f2f4e7777
tinagrit <mu@tinagrit.com>
2026-09-21 16:48:52 -0700

    extension set up

---
+++ background.js
@@ -0,0 +1,39 @@
+const MENU_COMMANDS = {
+  "accounting-helper-sum": "start-sum",
+  "accounting-helper-fill-zero": "start-fill-zero",
+};
+
+function installContextMenus() {
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: "accounting-helper-sum",
+      title: "Sum inputs",
+      contexts: ["editable"],
+    });
+
+    chrome.contextMenus.create({
+      id: "accounting-helper-fill-zero",
+      title: "Fill 0s",
+      contexts: ["editable"],
+    });
+  });
+}
+
+chrome.runtime.onInstalled.addListener(installContextMenus);
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const command = MENU_COMMANDS[info.menuItemId];
+
+  if (!command || typeof tab?.id !== "number") {
+    return;
+  }
+
+  chrome.tabs
+    .sendMessage(
+      tab.id,
+      { command },
+      { frameId: typeof info.frameId === "number" ? info.frameId : 0 },
+    )
+    .catch(() => {
+    });
+});
